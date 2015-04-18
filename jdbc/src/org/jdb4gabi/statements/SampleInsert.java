192e122bfa34d5e31170657d4b5348d32397f964
Jean Villete <brunopib@gmail.com>
2015-04-18 10:37:09 -0300

    Modificação na maneira como o SampleInsert registra informações na base de dados.

--- jdbc/src/org/jdb4gabi/statements/SampleInsert.java
+++ jdbc/src/org/jdb4gabi/statements/SampleInsert.java
@@ -4,29 +4,47 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import org.jdb4gabi.model.Estado;
+
 public class SampleInsert extends ConnectionData {
 
 	public static void main(String[] args) {
-		try {
-			SampleInsert si = new SampleInsert();
-			si.insert();
+		if ( args != null && args.length == 2 ) {
+			Estado estado = new Estado();
+			estado.setNome( args[0] );
+			estado.setUf( args[1] );
 			
-			System.out.println("Novo registro inserido.");
-		} catch (SQLException e) {
-			System.out.println("Problemas ao tentar inserir registro.");
-			e.printStackTrace();
+			try {
+				SampleInsert si = new SampleInsert();
+				si.insert( estado );
+				
+				System.out.println("Novo registro inserido.");
+			} catch (SQLException e) {
+				System.out.println("Problemas ao tentar inserir registro.");
+				usage();
+				
+				e.printStackTrace();
+			}
+		} else {
+			usage();
 		}
 	}
 
-	private void insert() throws SQLException{
+	private void insert( Estado estado ) throws SQLException{
 		Connection conn = this.getConnection();
-		PreparedStatement pstmt = conn.prepareStatement(" insert into estado( nome, uf ) values ( ?, ? ) ");
-		pstmt.setString(1, "Mato Grosso");
-		pstmt.setString(2, "MT");
+		PreparedStatement pstmt = conn.prepareStatement( " insert into estado( nome, uf ) values ( ?, ? ) " );
+		pstmt.setString( 1, estado.getNome() );
+		pstmt.setString( 2, estado.getUf() );
 		
 		pstmt.executeUpdate();
 		
 		pstmt.close();
 		conn.close();
 	}
+	
+	private static void usage() {
+		System.out.println( "SampleInsert - usage" );
+		System.out.println( "\tSampleInsert estadoNome estadoUf" );
+	}
+	
 }
